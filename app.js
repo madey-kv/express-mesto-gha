8ff767d7869dd1861bb4a01a03d7a3a12fb99377
Xenia Madey <98184809+madey-kv@users.noreply.github.com>
2022-09-17 16:20:47 +0300

    Проектная работа 13, правки 5

--- app.js
+++ app.js
@@ -23,11 +23,12 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 
+app.use(express.json());
+app.use('/users', routerUsers);
+app.use('/cards', routerCards);
 app.use((req, res) => {
   res.status(404).send({ message: "Роутер не найден!" });
 });
-app.use('/users', routerUsers);
-app.use('/cards', routerCards);
 
 
 app.listen(PORT, () => {
